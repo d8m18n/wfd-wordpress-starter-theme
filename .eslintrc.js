e46299ad4c9ab1ccc0ab2eb46c5b77b0b6ea2ce8
Damian Saunders <damian@webfoundations.com.au>
2020-07-14 19:19:19 +1000

    Initial prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
 	env: {
 		browser: true,
 		es2020: true,
+		node: true,
 	},
 	extends: [
 		'plugin:@wordpress/eslint-plugin/recommended',
 		'plugin:react/recommended',
+		'prettier',
 	],
 	parserOptions: {
 		ecmaFeatures: {
