14c9d1c6c3e009058c562de037ec78e65489190d
Damian Saunders <damian@webfoundations.com.au>
2020-08-05 10:26:43 +1000

    Continuing theme refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2020: true,
-		node: true,
-	},
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended',
-		'plugin:react/recommended',
-		'prettier',
-	],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 11,
-		sourceType: 'module',
-	},
-	plugins: [ 'react' ],
-	rules: {
-		'no-console': 'off',
-	},
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    extends: ["eslint:recommended", "prettier"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        "no-console": "warn"
+    }
 };
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp");
+const zip = require("gulp-zip");
+const replace = require("gulp-replace");
+const info = require("./package.json");
+const del = require("del");
+
+const themeName = info.name;
+const themeVersion = info.version;
+const theme = `${themeName}-${themeVersion}`;
+
+function clean(cr) {
+    del(["themeBundle"]);
+    return cr();
+}
+
+function bundle() {
+    return gulp
+        .src([
+            "**/*",
+            "!.vscode/**",
+            "!node_modules/**",
+            "!src/**",
+            "!bundled/**",
+            "!vendor/**",
+            "!.eslintignore",
+            "!.eslintrc",
+            "!.gitignore",
+            "!.stylelintrc",
+            "!composer.json",
+            "!composer.lock",
+            "!gulpfile.js",
+            "!package.json",
+            "!package-lock.json",
+            "!phpcs.xml",
+            "!postcss.config.js",
+            "!README.md",
+            "!webpack.dev.config.js",
+            "!webpack.production.config.js"
+        ])
+        .pipe(replace("wfd Dev Child", theme))
+        .pipe(zip(`${theme}.zip`))
+        .pipe(gulp.dest("themeBundle"));
+}
+exports.clean = clean;
+exports.bundle = bundle;
